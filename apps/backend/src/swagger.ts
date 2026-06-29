d76e650db8e11ea7654e458afbc0e61db6a31bc5
Baseer Afridi <baseergroot@gmail.com>
2026-06-29 12:33:19 +0500

    chore: ignore generated swagger output file in backend directory

--- apps/backend/src/swagger.ts
+++ apps/backend/src/swagger.ts
@@ -1,6 +1,24 @@
 import swaggerAutogen from 'swagger-autogen';
+import dotenv from 'dotenv';
+dotenv.config();
 
 const outputFilePath = 'public/swagger-output.json';
 const inputFile = 'routes/helper.ts';
 
-swaggerAutogen(outputFilePath, [inputFile], {}); 
+if (!process.env.BASE_URL) {
+  throw new Error("BASE_URL is not defined");
+}
+
+console.log(process.env.BASE_URL);
+
+swaggerAutogen(outputFilePath, [inputFile], {
+  title: "Ecom Monolith API",
+  description: "API Documentation for Ecom Monolith",
+  version: "1.0.0",
+  host: process.env.BASE_URL,
+  schemes: [`${process.env.NODE_ENV === 'production' ? 'https' : 'http'}`], // For localhost: http://, for production: https://
+  info: {
+    title: "Ecom Monolith API",
+    description: "API Documentation for Ecom Monolith",
+  },
+}); 
